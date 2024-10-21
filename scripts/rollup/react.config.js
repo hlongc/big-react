3abe00ad0620ca65f9b4caf68b7ca8e2c076d1e9
ronnie.hu <hulc_it@163.com>
2024-10-22 00:14:03 +0800

    feat: 完善打包逻辑

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,49 @@
+import { getPackageJSon, resolvePkgPath, getBaseRollupPlugin } from './utils';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJSon('react');
+const pkgPath = resolvePkgPath(name);
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	// react
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'index.js',
+			format: 'umd'
+		},
+		plugins: [
+			...getBaseRollupPlugin(),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, version, description }) => ({
+					name,
+					version,
+					description,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx-runtime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			},
+			// jsx-dev-runtime
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			}
+		],
+		plugins: getBaseRollupPlugin()
+	}
+];
---
+++ scripts/rollup/utils.js
@@ -0,0 +1,26 @@
+import path from 'path';
+import fs from 'fs';
+import ts from '@rollup/plugin-typescript';
+import cmj from '@rollup/plugin-commonjs';
+
+// 包源码的路径
+const pkgPath = path.resolve(__dirname, '../../packages');
+// 打包以后得路径
+const distPath = path.resolve(__dirname, '../../dist/node_modules');
+
+export function resolvePkgPath(name, isDist) {
+	if (isDist) {
+		return `${distPath}/${name}`;
+	}
+	return `${pkgPath}/${name}`;
+}
+
+export function getPackageJSon(name) {
+	const path = resolvePkgPath(name);
+	const str = fs.readFileSync(`${path}/package.json`, { encoding: 'utf8' });
+	return JSON.parse(str);
+}
+
+export function getBaseRollupPlugin({ typescript = {} } = {}) {
+	return [cmj(), ts()];
+}
